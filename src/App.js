e12411b929f152140f7fcdd0541bb415088f1116
Veena Sudarsanan Geetha <veenasg83@gmail.com>
2019-09-12 15:04:09 +1000

    redux counter app initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/counter.js';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const intitalState = { count: 0 };
+function reducer(state = intitalState, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - 1
+            };
+        default:
+            return state;
+    }
 }
+
+const store = createStore(reducer);
+
+const App = () => (
+    <Provider store={store}>
+
+        <Counter />
+    </Provider>
+
+);
+
 
 export default App;
